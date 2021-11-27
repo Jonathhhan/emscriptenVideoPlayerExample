9a90e53e4d4ef7b614567208ebe439171463ea6c
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2021-11-28 00:26:49 +0100

    Add files via upload

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,118 +1,271 @@
+/*
+ * Copyright (c) 2011 Dan Wilcox <danomatika@gmail.com>
+ *
+ * BSD Simplified License.
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ *
+ * See https://github.com/danomatika/ofxPd for documentation
+ *
+ */
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-	ofBackground(255,255,255);
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
 	ofSetVerticalSync(true);
-	isPaused = false;
-	isMousePressed = false;
+	//ofSetLogLevel("Pd", OF_LOG_VERBOSE); // see verbose info inside
 
-	// Uncomment this to show movies with alpha channels
-	// videoPlayer.setPixelFormat(OF_PIXELS_RGBA);
+	// double check where we are ...
+	cout << ofFilePath::getCurrentWorkingDirectory() << endl;
 
-	videoPlayer.setLoopState(OF_LOOP_NORMAL);
-	videoPlayer.load("dummy");
+	// the number of libpd ticks per buffer,
+	// used to compute the audio buffer len: tpb * blocksize (always 64)
+	#ifdef TARGET_LINUX_ARM
+		// longer latency for Raspberry PI
+		int ticksPerBuffer = 32; // 32 * 64 = buffer len of 2048
+		int numInputs = 0; // no built in mic
+	#else
+		int ticksPerBuffer = 1; // 8 * 64 = buffer len of 512
+		int numInputs = 0;
+	#endif
+
+	// setup OF sound stream
+	ofSoundStreamSettings settings;
+	settings.numInputChannels = 2;
+	settings.numOutputChannels = 2;
+	settings.sampleRate = 44100;
+	settings.bufferSize = ofxPd::blockSize() * ticksPerBuffer;
+	settings.setInListener(this);
+	settings.setOutListener(this);
+	ofSoundStreamSetup(settings);
+
+	// setup Pd
+	//
+	// set 4th arg to true for queued message passing using an internal ringbuffer,
+	// this is useful if you need to control where and when the message callbacks
+	// happen (ie. within a GUI thread)
+	//
+	// note: you won't see any message prints until update() is called since
+	// the queued messages are processed there, this is normal
+	//
+	if(!pd.init(2, numInputs, 44100, ticksPerBuffer, false)) {
+		OF_EXIT_APP(1);
+	}
+
+	midiChan = 1; // midi channels are 1-16
+
+	// subscribe to receive source names
+	pd.subscribe("toOF");
+	pd.subscribe("env");
+
+	// add message receiver, required if you want to recieve messages
+	pd.addReceiver(*this); // automatically receives from all subscribed sources
+	pd.ignoreSource(*this, "env");        // don't receive from "env"
+	//pd.ignoreSource(*this);             // ignore all sources
+	//pd.receiveSource(*this, "toOF");	  // receive only from "toOF"
+
+	// add midi receiver, required if you want to recieve midi messages
+	pd.addMidiReceiver(*this); // automatically receives from all channels
+	//pd.ignoreMidiChannel(*this, 1);     // ignore midi channel 1
+	//pd.ignoreMidiChannel(*this);        // ignore all channels
+	//pd.receiveMidiChannel(*this, 1);    // receive only from channel 1
+
+	// add the data/pd folder to the search path
+	// pd.addToSearchPath("pd/abs");
+
+	// audio processing on
+	pd.start();
+
+	// -----------------------------------------------------
+	cout << endl << "BEGIN Patch Test" << endl;
+
+	// open patch
+	patch = pd.openPatch("pd/test.pd");
+	cout << patch << endl;
+	
+	cout << "FINISH Patch Test" << endl;
+
+
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-    videoPlayer.update();
-    texture = videoPlayer.getTexture();
+void ofApp::update() {
+	ofBackground(100, 100, 100);
+	
+	// since this is a test and we don't know if init() was called with
+	// queued = true or not, we check it here
+	if(pd.isQueued()) {
+		// process any received messages, if you're using the queue and *do not*
+		// call these, you won't receieve any messages or midi!
+		pd.receiveMessages();
+		pd.receiveMidi();
+	}
+
+	// update scope array from pd
+
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
-    ofSetHexColor(0xFFFFFF);
-    if (texture.isAllocated()){
-    texture.draw(20,20);
-    }
-    ofSetColor(0,10,240);
-    ofDrawRectangle(500, 320, 100, 100);
-    ofSetHexColor(0x000000);
+void ofApp::draw() {
 
-    ofDrawBitmapString("duration: " + ofToString(videoPlayer.getPosition()*videoPlayer.getDuration(),2) + "/"+ofToString(videoPlayer.getDuration(),2), 20, 450);
-    ofDrawBitmapString("press space for pause", 20, 500);
-    ofDrawBitmapString("click or drag mouse for position", 20, 550);
-    ofDrawBitmapString("press mouse on blue rectangle for file browser", 20, 600);
-    ofDrawBitmapString("drag video on window", 20, 650);
-    
-    if(videoPlayer.getIsMovieDone()){
-        ofSetHexColor(0xFF0000);
-        ofDrawBitmapString("end of movie",20,440);
-    }
+
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-	if (key == 32){
-	isPaused = !isPaused;
-	videoPlayer.setPaused(isPaused);
+void ofApp::exit() {
+
+	// cleanup
+	ofSoundStreamStop();
+}
+
+//--------------------------------------------------------------
+void ofApp::playTone(int pitch) {
+	pd << StartMessage() << "pitch" << pitch << FinishList("tone") << Bang("tone");
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed (int key) {
+
+	switch(key) {
+		
+		// musical keyboard
+		case 'a':
+			playTone(60);
+			break;
+		case 'w':
+			playTone(61);
+			break;
+		case 's':
+			playTone(62);
+			break;
+		case 'e':
+			playTone(63);
+			break;
+		case 'd':
+			playTone(64);
+			break;
+		case 'f':
+			playTone(65);
+			break;
+		case 't':
+			playTone(66);
+			break;
+		case 'g':
+			playTone(67);
+			break;
+		case 'y':
+			playTone(68);
+			break;
+		case 'h':
+			playTone(69);
+			break;
+		case 'u':
+			playTone(70);
+			break;
+		case 'j':
+			playTone(71);
+			break;
+		case 'k':
+			playTone(72);
+			break;
+
+		case ' ':
+			if(pd.isReceivingSource(*this, "env")) {
+				pd.ignoreSource(*this, "env");
+				cout << "ignoring env" << endl;
+			}
+			else {
+				pd.receiveSource(*this, "env");
+				cout << "receiving from env" << endl;
+			}
+			break;
+
+		default:
+			break;
 	}
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::audioReceived(float * input, int bufferSize, int nChannels) {
+	pd.audioIn(input, bufferSize, nChannels);
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
+void ofApp::audioRequested(float * output, int bufferSize, int nChannels) {
+	pd.audioOut(output, bufferSize, nChannels);
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-	if (isMousePressed){
-        	int width = ofGetWidth();
-        	float pct = (float)x / (float)width;
-        	videoPlayer.setPosition(pct);
-        }
+void ofApp::print(const std::string &message) {
+	cout << message << endl;
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-	isMousePressed = true;
-	if(isMousePressed){
-        	videoPlayer.setPaused(true);
+void ofApp::receiveBang(const std::string &dest) {
+	cout << "OF: bang " << dest << endl;
+}
+
+void ofApp::receiveFloat(const std::string &dest, float value) {
+	cout << "OF: float " << dest << ": " << value << endl;
+}
+
+void ofApp::receiveSymbol(const std::string &dest, const std::string &symbol) {
+	cout << "OF: symbol " << dest << ": " << symbol << endl;
+}
+
+void ofApp::receiveList(const std::string &dest, const pd::List &list) {
+	cout << "OF: list " << dest << ": ";
+
+	// step through the list
+	for(int i = 0; i < list.len(); ++i) {
+		if(list.isFloat(i))
+			cout << list.getFloat(i) << " ";
+		else if(list.isSymbol(i))
+			cout << list.getSymbol(i) << " ";
 	}
-	int width = ofGetWidth();
-	float pct = (float)x / (float)width;
-	videoPlayer.setPosition(pct);
-	if (x >= 500 && x <= 600 && y >= 320 && y <= 420){
-		isMousePressed = false;
-		videoPlayer.load("dummy");
-	}
+
+	// you can also use the built in toString function or simply stream it out
+	// cout << list.toString();
+	// cout << list;
+
+	// print an OSC-style type string
+	cout << list.types() << endl;
+}
+
+void ofApp::receiveMessage(const std::string&dest, const std::string &msg, const pd::List &list) {
+	cout << "OF: message " << dest << ": " << msg << " " << list.toString() << list.types() << endl;
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-	isMousePressed = false;
-	if(!isMousePressed){
-        	videoPlayer.setPaused(false);
-	}
+void ofApp::receiveNoteOn(const int channel, const int pitch, const int velocity) {
+	cout << "OF MIDI: note on: " << channel << " " << pitch << " " << velocity << endl;
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
+void ofApp::receiveControlChange(const int channel, const int controller, const int value) {
+	cout << "OF MIDI: control change: " << channel << " " << controller << " " << value << endl;
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
+// note: pgm nums are 1-128 to match pd
+void ofApp::receiveProgramChange(const int channel, const int value) {
+	cout << "OF MIDI: program change: " << channel << " " << value << endl;
 }
 
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
+void ofApp::receivePitchBend(const int channel, const int value) {
+	cout << "OF MIDI: pitch bend: " << channel << " " << value << endl;
 }
 
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
+void ofApp::receiveAftertouch(const int channel, const int value) {
+	cout << "OF MIDI: aftertouch: " << channel << " " << value << endl;
 }
 
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::receivePolyAftertouch(const int channel, const int pitch, const int value) {
+	cout << "OF MIDI: poly aftertouch: " << channel << " " << pitch << " " << value << endl;
+}
 
+// note: pd adds +2 to the port num, so sending to port 3 in pd to [midiout],
+//       shows up at port 1 in ofxPd
+void ofApp::receiveMidiByte(const int port, const int byte) {
+	cout << "OF MIDI: midi byte: " << port << " " << byte << endl;
 }
